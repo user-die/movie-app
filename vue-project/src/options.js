8bb94aa600da954fb36ae90ff1276cb48e914274
userdie <vadim.vadimov123@mail.ru>
2024-07-21 02:01:09 +0500

    configuring deployment

--- vue-project/src/options.js
+++ vue-project/src/options.js
@@ -1,5 +1,7 @@
-console.log(process.env.VERCEL_key1)
-console.log(process.env.key1)
+console.log(import.meta.env.VITE_key1)
+console.log(import.meta.env)
+console.log(import.meta.env.VERCEL_VITE_key1)
+console.log(import.meta.env.VERCEL_key1)
 
 export default {
   request1: {

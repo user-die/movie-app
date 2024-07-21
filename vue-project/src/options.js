0ba8565eed4bd10f27bdb35fd75f8321ab8aa901
userdie <vadim.vadimov123@mail.ru>
2024-07-21 12:23:49 +0500

    configuring deployment

--- vue-project/src/options.js
+++ vue-project/src/options.js
@@ -1,15 +1,10 @@
-console.log(import.meta.env.VITE_key1)
-console.log(import.meta.env)
-console.log(import.meta.env.VERCEL_VITE_key1)
-console.log(import.meta.env.VERCEL_key1)
-
 export default {
   request1: {
     method: 'GET',
-    headers: { accept: 'application/json', 'X-API-KEY': 'process.env.key1' }
+    headers: { accept: 'application/json', 'X-API-KEY': process.env.key1 }
   },
   request2: {
     method: 'GET',
-    headers: { accept: 'application/json', 'X-API-KEY': 'process.env.key2' }
+    headers: { accept: 'application/json', 'X-API-KEY': process.env.key2 }
   }
 }
